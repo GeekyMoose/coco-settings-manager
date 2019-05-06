5d4931da241a62b79aab616370005be4522d038c
geekymoose <constantin.masson@umontreal.ca>
2019-05-07 00:36:25 +0200

    Update cmd crea_sh_ln to use prefix set in the config file and update doc

--- scripts/settings-gen.sh
+++ scripts/settings-gen.sh
@@ -8,33 +8,35 @@
 # Constants
 # -----------------------------------------------------------------------------
 COLOR_NORMAL="\033[0m"
+COLOR_GRAY="\033[30m"
 COLOR_RED="\033[31m"
 COLOR_GREEN="\033[32m"
 COLOR_YELLOW="\033[33m"
-COLOR_GRAY="\033[30m"
-# Global vars
-ERROR_COUNTER=0
-SCRIPT_NAME=${0##*/} # Removes ./ before script name
-FLAGS_LN=
+COLOR_BLUE="\033[34m"
 
 # Settings location
 CONFIG_DIR="${HOME}/.config/coco/" # Where to search
 CONFIG_EXT="gen.conf" # File ext to process
 
+# Intermal vars
+ERROR_COUNTER=0
+SCRIPT_NAME=${0##*/} # Removes ./ before script name
+FLAGS_LN=
+
 
 # ------------------------------------------------------------------------------
-# Args and Options
+# Args and options
 # ------------------------------------------------------------------------------
 show_usage_and_exit() {
     echo "Usage: $SCRIPT_NAME [OPTION]..."
-    echo "Generate the configuration decribed by the configuration files."
+    echo "Generates the configuration decribed by the configuration files."
     echo "Looks for all config files under $CONFIG_DIR directory."
     echo "Config file is any file that end with extension $CONFIG_EXT."
     echo "  -h  Show this help."
-    echo "  -c  Use given file instead of default config files."
-    echo "  -f  Force create link even if already exists (WARNING: delete existing file)."
+    echo "  -c  Use a specific file instead of default one."
+    echo "  -f  Force create link if already exists (WARNING: delete existing file)."
     echo ""
-    echo "Default behavior is not to force symlinks creation."
+    echo "The default behavior is not to force symlinks creation."
     echo "If a file already exists, it won't be altered."
     echo "Use -f to force link creation (Warning: totally delete the old one)"
     exit 0
@@ -48,12 +50,12 @@ show_usage_and_exit() {
 process_options() {
     while getopts ":hc:f" optname; do
         case $optname in
-            h) show_usage_and_exit ;; # Help
-            f) FLAGS_LN='-f' ;; # Force
-            c) config_file=$OPTARG ;; # Use config file
-            ?) show_usage_and_exit ;; # Unknown option
-            :) show_usage_and_exit ;; # Argument missing for option
-            *) show_usage_and_exit ;; # Should never occur anyway
+            h) show_usage_and_exit ;;   # Help
+            f) FLAGS_LN='-f' ;;         # Force
+            c) config_file=$OPTARG ;;   # Use config file
+            ?) show_usage_and_exit ;;   # Unknown option
+            :) show_usage_and_exit ;;   # Argument missing for option
+            *) show_usage_and_exit ;;   # Should never occur anyway
         esac
     done
     return $OPTIND
@@ -61,23 +63,23 @@ process_options() {
 
 
 # ------------------------------------------------------------------------------
-# Print Functions
+# Print methods
 # ------------------------------------------------------------------------------
 
 print_title() {
-    echo -e "*** $1 ***"
+    echo -e "---> $1"
 }
 print_error() {
-    echo -e "   ${COLOR_RED}*[ERROR] ${1}${COLOR_NORMAL}"
+    echo -e "${COLOR_RED}[ERROR] ${1}${COLOR_NORMAL}"
 }
 print_warning() {
-    echo -e "    ${COLOR_YELLOW}[Warning] ${1}${COLOR_NORMAL}"
+    echo -e "${COLOR_YELLOW}[WARNING] ${1}${COLOR_NORMAL}"
 }
 print_success() {
-    echo -e "    ${COLOR_GREEN}[SUCCESS]${COLOR_NORMAL} $1"
+    echo -e "${COLOR_GREEN}[SUCCESS]${COLOR_NORMAL} $1"
 }
 print_info() {
-    echo -e "    ${COLOR_GRAY}[INFO]${COLOR_NORMAL} $1"
+    echo -e "${COLOR_GRAY}[INFO]${COLOR_NORMAL} $1"
 }
 
 # Print a message according to given error code.
@@ -95,24 +97,24 @@ show_err_status() {
 
 
 # ------------------------------------------------------------------------------
-# Assets Functions
+# Commands
 # ------------------------------------------------------------------------------
 
-# Create a symlink.
+# Creates a symlink.
 #
-# \param 1 Source file (Absolute path).
-# \param 2 Symlink file (Absolute path).
+# \param 1 Source file (absolute path).
+# \param 2 Symlink file (absolute path).
 crea_ln() {
     # File to link must exists
     if [ ! -e "$1" ]; then
         ERROR_COUNTER=$((ERROR_COUNTER + 1))
-        print_error "$1 doesn't exists..."
+        print_error "$1 doesn't exists"
         return 42
     fi
-    # Only link to file allowed (Not folder)
-    if [ ! -f "$1" ];then
+    # Only link to file allowed (not folder)
+    if [ ! -f "$1" ]; then
         ERROR_COUNTER=$((ERROR_COUNTER + 1))
-        print_error "$1 is not a file type (Only files allowed for symlinks)"
+        print_error "$1 is not a file (only files allowed for symlinks)"
         return 42
     fi
     msg=$(ln -sv $FLAGS_LN "$1" "$2" 2>&1)
@@ -120,44 +122,49 @@ crea_ln() {
     return 0
 }
 
-# Create a repertory if doesn't exists.
+# Creates a repertory.
+# Does nothing if it already exists.
 #
-# \params 1 Folder to create (Full path).
+# \params 1 Folder to create (full path).
 crea_rep() {
     if [ -e "$1" ] && [ ! -d "$1" ]; then
         ERROR_COUNTER=$((ERROR_COUNTER + 1))
-        print_error "$1 already exists and is a file"
+        print_error "$1 already exists but is a file"
         return 1
     fi
     if [ -e "$1" ] && [ -d "$1" ]; then
-        print_info "folder $1 already exists"
+        print_info "$1 already exists"
         return 1
     fi
     msg=$(mkdir -pv "$1" 2>&1)
     show_err_status "$msg"
 }
 
-# Browser scripts folder and create the link with specific renaming rule.
+# Browses a folder and creates the link to each files inside.
+# This is meant to be used for folder with only scripts inside.
+# Removes .sh file extension and add the given prefix.
+# (e.g., Prefix 'perso-' on file 'testX.sh' gives 'perso-testX')
 #
 # \param 1 Source folder.
 # \param 2 Destination folder.
+# \param 3 Prefix to add on file symlinks
 crea_sh_ln() {
     # Script folder must exists
     if ! [ -e "$1" ]; then
         ERROR_COUNTER=$((ERROR_COUNTER + 1))
-        print_error "$1 doesn't exists..."
+        print_error "$1 doesn't exists"
         return 42
     fi
     scripts=$(ls "$1")
     for f in $scripts; do
         name=${f%.sh} # Removes sh extension
-        name="zozo-$name"
+        name="${3}$name"
         crea_ln "$1/$f" "$2/$name" # Folder case handled in crea_ln
     done
 }
 
-# Browser the current folder and link each files inside.
-# This does not link the folder itself (But its content only).
+# Browses the current folder and links each files inside.
+# This does not link the folder itself but its content only.
 #
 # \param 1 Source folder.
 # \param 2 Destination folder.
@@ -165,7 +172,7 @@ crea_rep_ln() {
     # Folder must exists
     if ! [ -e "$1" ]; then
         ERROR_COUNTER=$((ERROR_COUNTER + 1))
-        print_error "$1 doesn't exists..."
+        print_error "$1 doesn't exists"
         return 42
     fi
     file=$(ls "$1")
@@ -176,12 +183,12 @@ crea_rep_ln() {
     done
 }
 
-# Execute a shell command.
+# Executes a shell command.
 #
 # \param 1 The shell command to execute.
 exec_cmd() {
     msg=$(eval "$1")
-    show_err_status "Execute '$1' $msg"
+    show_err_status "Executing '$1' ${msg}..."
 }
 
 
@@ -189,7 +196,7 @@ exec_cmd() {
 # Parse functions
 # ------------------------------------------------------------------------------
 
-# Parse one line of the config file.
+# Parses one line from the config file.
 #
 # \param 1 The line to parse.
 parse_line() {
@@ -197,30 +204,31 @@ parse_line() {
     if [ -z "$line" ]; then
         return 1
     fi
-    # " are removed in case of user write path in config as "the/path"
-    # The " would create strange behavior (Element created at $PWD)
+    # Chars " are removed in case user writes path in config like "the/path"
+    # The " would create unexpected behavior (element created at $PWD)
     command=$(echo "$line"  | tr -d '"' | tr -s ' ' | cut -d ' ' -f 1)
     params=$(echo "$line"   | tr -d '"' | tr -s ' ' | cut -d ' ' -f 2-)
     param1=$(echo "$line"   | tr -d '"' | tr -s ' ' | cut -d ' ' -f 2)
     param2=$(echo "$line"   | tr -d '"' | tr -s ' ' | cut -d ' ' -f 3)
+    param3=$(echo "$line"   | tr -d '"' | tr -s ' ' | cut -d ' ' -f 4)
 
-    # Eval required to reevaluate var (like $HOME) to its actual value
+    # Eval required to re-evaluate vars to its actual value (i.e., $HOME).
     source=$(eval echo $param1)
     dest=$(eval echo $param2)
 
-    # Action according to command value
+    # Exec command
     case "$command" in
         'crea_ln')      crea_ln "$source" "$dest" ;;
         'crea_rep')     crea_rep "$source" ;;
         'crea_rep_ln')  crea_rep_ln "$source" "$dest" ;;
-        'crea_sh_ln')   crea_sh_ln "$source" "$dest" ;;
+        'crea_sh_ln')   crea_sh_ln "$source" "$dest" "$param3" ;;
         'show_title')   print_title "$params" ;;
         'show_warning') print_warning "$params" ;;
         'execute')      exec_cmd "$params" ;;
     esac
 }
 
-# Parse the entire file.
+# Parses an entire file.
 #
 # \param 1 File to parse.
 parse_file() {
@@ -229,14 +237,15 @@ parse_file() {
     done < "$1"
 }
 
-# Parse all files with given extension in the path given.
+# Parses all files with the given extension and located in the path given.
 #
 # \param 1 Path where to search.
 # \param 2 File extension.
 parse_all_files() {
     list_files=$(find "$1" -name "*.$2" 2>&1)
     if [ -z "$list_files" ]; then
-        echo -e "${COLOR_RED}[ERROR] $1 does not contains any config files (*.${2})${COLOR_NORMAL}"
+        ERROR_COUNTER=$((ERROR_COUNTER + 1))
+        print_error "$1 does not contain any config files (*.${2})"
         return 42
     fi
     for file in $list_files ;do
@@ -251,32 +260,30 @@ parse_all_files() {
 # ------------------------------------------------------------------------------
 process_options "$@"
 
-echo "-------------------------------------------------------------------------"
-echo "Settings configuration"
-echo "-------------------------------------------------------------------------"
+echo "---> Generating coco-settings configuration..."
 
 # If file given in parameter, process it instead of default behavior
 if [ ! -z $config_file ]; then
-    # Config file must exists and be a file
+    # Config file must exists
     if [ ! -f "$config_file" ];then
-        echo -e "${COLOR_RED}[ERROR] ${config_file} does exists or is not a file...${COLOR_NORMAL}"
+        print_error "${config_file} does exists or is not a file"
         exit 42
     fi
     parse_file "$config_file"
 else
-    # Config dir must exists
+    # Config folder must exists
     if [ ! -d "$CONFIG_DIR" ];then
-        echo -e "${COLOR_RED}[ERROR] ${CONFIG_DIR} does exists or is not a folder...${COLOR_NORMAL}"
+        print_error "${CONFIG_DIR} does exists or is not a folder"
         exit 42
     fi
     parse_all_files "$CONFIG_DIR" "$CONFIG_EXT"
 fi
 
 
-echo "-------------------------------------------------------------------------"
 if [ $ERROR_COUNTER -ne 0 ]; then
-    echo -e " $ERROR_COUNTER Errors"
+    print_error "$ERROR_COUNTER errors"
 else
-    echo -e " Finished successfully"
+    print_success "Script finished successfully"
 fi
-echo "-------------------------------------------------------------------------"
+
+
